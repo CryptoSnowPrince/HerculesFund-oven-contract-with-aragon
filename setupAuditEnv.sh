dcddc2b63c80a5f0e89994897d923e183e63c31f
DESKTOP-3J65I2E\admin <topdirector2017@gmail.com>
2022-01-27 13:04:41 +0900

    setupAuditEnv.sh

---
+++ setupAuditEnv.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+sudo su &
+echo hello1
+yarn install --force;
+echo hello2;
+npm audit fix --force;
+echo hello3;
+/home/gitpod/.pyenv/versions/3.8.12/bin/python3 -m pip install --upgrade pip;
+echo hello4;
+pip3 install solc-select;
+echo hello5;
+pip3 install slither-analyzer;
+echo hello6;
+solc-select install;
+echo hello7
